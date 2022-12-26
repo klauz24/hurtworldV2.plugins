695a69edef016af233c5134cb890c288ad2a7c37
klauz24 <klauz24@outlook.com>
2022-12-26 12:13:24 +0000

    Update AntiActiveSession.cs

--- AntiActiveSession.cs
+++ AntiActiveSession.cs
@@ -1,22 +1,19 @@
-﻿namespace Oxide.Plugins
+namespace Oxide.Plugins
 {
-	// This plugin might cause issues, use it on your own risk!
-
-	[Info("Anti Active Session", "klauz24", "1.0.0")]
+	[Info("Anti Active Session", "klauz24", "1.0.1")]
 	internal class AntiActiveSession : HurtworldPlugin
 	{
 		private void OnServerInitialized()
 		{
-			timer.Every(3f, () =>
+			timer.Every(5f, () =>
 			{
-				foreach(var session in GameManager.Instance._steamIdSession.Values)
-                {
+				foreach (var session in GameManager.Instance._steamIdSession.Values)
+				{
 					if (session != null && !session.IsLoaded)
-                    {
-						// This player is not on the server but has active session, removing it.
+					{
 						GameManager.Instance._steamIdSession.Remove(session.SteamId);
 					}
-                }
+				}
 			});
 		}
 	}
