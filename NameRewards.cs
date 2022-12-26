01135aa79506f99d3b24df671198199e7d364ea1
klauz24 <klauz24@outlook.com>
2022-12-26 12:12:09 +0000

    Update NameRewards.cs

--- NameRewards.cs
+++ NameRewards.cs
@@ -1,39 +1,52 @@
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using Oxide.Core.Libraries.Covalence;
 
 namespace Oxide.Plugins
 {
-    [Info("Name Rewards", "klauz24", "1.0.0")]
+    [Info("Name Rewards", "klauz24", "1.0.1")]
     internal class NameRewards : CovalencePlugin
     {
-        private const string _pluginPerm = "namerewards.use";
+        private const string _perm = "namerewards.use";
 
-        protected override void LoadDefaultConfig()
+        private PluginConfig _config;
+
+        private class PluginConfig
         {
-            Config["Words"] = new List<string>()
-            {
-                "SomeWordHereForRewards"
+            [JsonProperty("Words")]
+            public List<string> Words;
+        }
+
+        protected override void LoadDefaultConfig() => Config.WriteObject(GetDefaultConfig(), true);
+
+        private PluginConfig GetDefaultConfig()
+        {
+            return new PluginConfig()
+            { 
+                Words = new List<string>() { "SomeWordHere" }
             };
         }
 
-        private void Init() => permission.RegisterPermission(_pluginPerm, this);
+        private void Init()
+        {
+            _config = Config.ReadObject<PluginConfig>();
+            permission.RegisterPermission(_perm, this);
+        }
 
         private void OnUserConnected(IPlayer player)
         {
-            foreach (var str in Config["Words"] as List<string>)
+            foreach (var str in _config.Words)
             {
                 var match = player.Name.ToLower().Contains(str.ToLower());
-                if (match && !permission.UserHasPermission(player.Id, _pluginPerm))
+                if (match && !permission.UserHasPermission(player.Id, _perm))
                 {
-                    permission.GrantUserPermission(player.Id, _pluginPerm, this);
-                    Puts($"Granting plugin permission to {player.Name}");
+                    permission.GrantUserPermission(player.Id, _perm, this);
                 }
                 else
                 {
-                    if (permission.UserHasPermission(player.Id, _pluginPerm))
+                    if (permission.UserHasPermission(player.Id, _perm))
                     {
-                        permission.RevokeUserPermission(player.Id, _pluginPerm);
-                        Puts($"Revoking plugin permission from {player.Name}");
+                        permission.RevokeUserPermission(player.Id, _perm);
                     }
                 }
             }
